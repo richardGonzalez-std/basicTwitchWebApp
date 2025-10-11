ead16c7c374d6a014b1b9f5c91cad0a58564648c
Richard Gonzalez <zambranoa998@gmail.com>
2025-10-11 18:38:07 -0400

    security commit

--- basicTwitchWebApp/settings.py
+++ basicTwitchWebApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import hashlib
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,15 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#zgxg&$tvz-xo_p-3!3^l!gt@930zr#is$ci+9@m)pt77pk!wb'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-CLIENT_ID ="i4dllv4q6b9dpt2mglwbaw0gfz8139"
-CLIENT_SECRET = "wgyngeafws60ixdgi1et6x4fjjq1gq"
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 REDIRECT_URI = "http://localhost:8000/validate/"
 # Application definition
